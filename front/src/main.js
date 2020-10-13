08c1d9f455e8cee37e28c1fbe6ab0d6289157364
Setraniaina <setra.rasolomanana@gmail.com>
2020-10-13 12:09:41 +0200

    install npm router

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
+Vue.use(VueRouter)
 
 Vue.use(VueMaterial)
 Vue.config.productionTip = false
