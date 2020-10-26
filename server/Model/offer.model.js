ca4b192fcdc78c3ff862b4b422f4920543f5122f
ibrahima <ibrahimakd00@gmail.com>
2020-10-26 15:53:49 +0100

    Gestion des offres

--- server/Model/offer.model.js
+++ server/Model/offer.model.js
@@ -18,13 +18,13 @@ class Offer {
     }
 
     static async create (offer) {
-
+        console.log(offer)
         const result = await PostgresStore.client.query({
             text: `INSERT INTO ${Offer.tableName}
                     (name, price, description, image, id_restaurant, id_promo)
                     VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
                     values : [
-                        offer.name, offer.price, offer.description, offer.image, offer.idRestaurant, offer.idPromo
+                        offer.name, Number(offer.price), offer.description, offer.image, offer.idRestaurant, offer.idPromo
                     ]
         })
         return result.rows[0]
@@ -35,6 +35,45 @@ class Offer {
             text: `SELECT * FROM ${Offer.tableName}
             WHERE id_restaurant=$1`,
             values: [restaurantId]
+        })
+        return result.rows
+    }
+
+    static async delete(offerD){
+        const result = await PostgresStore.client.query({
+            text: `DELETE FROM ${Offer.tableName}
+            WHERE id=$1`,
+            values: [Number(offerD)]
+        })
+        return result.rows
+    }
+
+    static async modifName(offerM){
+        const result = await PostgresStore.client.query({
+            text: `UPDATE ${Offer.tableName}
+            SET name=$1
+            WHERE id=$2`,
+            values : [offerM.name, Number(offerM.id)]
+        })
+        return result.rows
+    }
+
+    static async modifPrice(offerP){
+        const result = await PostgresStore.client.query({
+            text: `UPDATE ${Offer.tableName}
+            SET price=$1
+            WHERE id=$2`,
+            values : [offerP.price, Number(offerP.id)]
+        })
+        return result.rows
+    }
+
+    static async modifDes(offerD){
+        const result = await PostgresStore.client.query({
+            text: `UPDATE ${Offer.tableName}
+            SET description=$1
+            WHERE id=$2`,
+            values : [offerD.description, Number(offerD.id)]
         })
         return result.rows
     }
--- server/route/offer.route.js
+++ server/route/offer.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const Offer = require("../model/offer.model")
 const PostgresStore = require("../PostgresStore")
+const hasToBeAuthenticated = require('../middlewares/has-to-be-authenticated.middleware')
 
 router.get('/offer/:id', async (req,res)=>{
 
@@ -8,4 +9,30 @@ router.get('/offer/:id', async (req,res)=>{
     res.json(result)
 })
 
+router.post('/offer', hasToBeAuthenticated, async (req,res)=>{
+    const result = await Offer.create(req.body)
+    res.json(result)
+})
+
+router.delete('/offer', hasToBeAuthenticated, async (req,res)=>{
+    const result = await Offer.delete(req.body.id)
+    res.json(result)
+})
+
+router.put('/offer/modifN', hasToBeAuthenticated, async (req,res)=>{
+    const result = await Offer.modifName(req.body)
+    res.json(result)
+})
+
+router.put('/offer/modifP', hasToBeAuthenticated, async (req,res)=>{
+    const result = await Offer.modifPrice(req.body)
+    res.json(result)
+})
+
+router.put('/offer/modifD', hasToBeAuthenticated, async (req,res)=>{
+    const result = await Offer.modifDes(req.body)
+    res.json(result)
+})
+
 module.exports = router
+
