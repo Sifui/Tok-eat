8998c6968349f3e980e6fa35ab5da5b9147b0df3
Leo.g <Leo.g@Univers1000.FR>
2020-10-28 19:34:03 +0100

    lil css

--- front/src/components/DisplayOffers.vue
+++ front/src/components/DisplayOffers.vue
@@ -1,20 +1,17 @@
 <template>
-  <div>
-        <div v-for="offer in offers" :key="offer.id" class='border'>
-            <div v-if="modal">
-                
-            </div>
-            <div v-on:click="displayOffer()" class='border'>
+    <div calss="flex">
+        <div v-for="offer in offers" :key="offer.id" class="border">
+            <div v-on:click="displayOffer()" class='offer'>
                 {{offer.name}}<br>
                 {{offer.price}} €<br>
                 {{offer.description}}<br>
             </div>
-            <div class='border'>
-                <button >Modifier</button>
-                <button v-on:click="deleteOffer(offer)" >Supprimer</button>
+            <div class='buttons'>
+                <button>Modifier</button>
+                <button v-on:click="deleteOffer(offer)">Supprimer</button>
             </div>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -52,19 +49,28 @@ export default {
 <style>
 .border{
     display: flex;
+    flex: 1 1 auto;
     border:1px solid black;
+    width: 100%;
+    text-align: center; 
 }
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
+.flex{
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 auto;
 }
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
+.offer{
+    border:1px solid black;
+    width: 80%;
+    text-align: center;
+}
+.buttons{
+    border:1px solid black;
+    width: 20%;
+    text-align: center;
+}
+button{
+    height:100%;
+    width: 50%;
 }
 </style>
